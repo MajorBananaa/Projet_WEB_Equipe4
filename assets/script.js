57b5452991f8cd6a57418c617352693ab7c0a4f0
RayaneHassani <rayane.hassani@viacesi.fr>
2025-03-14 18:11:46 +0100

    Home page refonte complète + changement CG

--- assets/script.js
+++ assets/script.js
@@ -83,3 +83,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     filterResults();
 });
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('menu-line'); 
+    navlist.classList.toggle('open')
+}
