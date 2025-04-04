e982ee8d6120f2cd31f61c68dcaa87973cb0e4cd
Theo-th <theo.thivillier@viacesi.fr>
2025-04-04 10:34:00 +0200

    specialchar et correction

--- src/Controllers/SearchController.php
+++ src/Controllers/SearchController.php
@@ -50,56 +50,63 @@ class SearchController {
         $dbOffer = new Offer();
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (isset($_POST["motivation"]) && isset($_FILES["cv"]) && isset($_POST["offer_id"])) {
-
+        
                 $check_file = new UploadService($_FILES["cv"], 10485760, ['application/pdf'], 'fichier/cv/');
                 $chemin_cv = $check_file->execute();
-
+        
                 $candidature = new Candidature();
-                $candidature->add([$_POST["offer_id"], $_SESSION["user_id"], $chemin_cv, $_POST["motivation"]]);
-
+                $candidature->add([
+                    htmlspecialchars($_POST["offer_id"]), 
+                    $_SESSION["user_id"], 
+                    $chemin_cv, 
+                    htmlspecialchars($_POST["motivation"])
+                ]);
+            
             } elseif (isset($_POST["id-supr"]) && isset($_POST["remove"])) {
-                $dbOffer->remove($_POST["id-supr"]);
+                $dbOffer->remove(htmlspecialchars($_POST["id-supr"]));
+            
             } elseif (isset($_POST["add"])) {
                 $offre = [
-                    'titre' => $_POST['titre'] ?? 'Titre par défaut',
-                    'description' => $_POST['description'] ?? 'Description par défaut',
-                    'salaire' => $_POST['salaire'] ?? 0,
-                    'teletravail' => $_POST['teletravail'] ?? 0,
-                    'duree' => $_POST['duree'] ?? 0,
-                    'id_etude' => $_POST['id_etude'] ?? 0,
-                    'id_contrat' => $_POST['id_contrat'] ?? 0,
-                    'id_secteur' => $_POST['id_secteur'] ?? 0,
-                    'id_entreprise' => $_POST['id_entreprise'] ?? 0
+                    'titre' => htmlspecialchars($_POST['titre'] ?? 'Titre par défaut'),
+                    'description' => htmlspecialchars($_POST['description'] ?? 'Description par défaut'),
+                    'salaire' => htmlspecialchars($_POST['salaire'] ?? 0),
+                    'teletravail' => htmlspecialchars($_POST['teletravail'] ?? 0),
+                    'duree' => htmlspecialchars($_POST['duree'] ?? 0),
+                    'id_etude' => htmlspecialchars($_POST['id_etude'] ?? 0),
+                    'id_contrat' => htmlspecialchars($_POST['id_contrat'] ?? 0),
+                    'id_secteur' => htmlspecialchars($_POST['id_secteur'] ?? 0),
+                    'id_entreprise' => htmlspecialchars($_POST['id_entreprise'] ?? 0)
                 ];
-
                 $dbOffer->add($offre);
+            
             } elseif (isset($_POST["update"])) {
-
                 $offre = [
-                    'id_offres' => $_POST['offer_id-upd'],
-                    'titre' => $_POST['titre'] ?? 'Titre par défaut',
-                    'description' => $_POST['description'] ?? 'Description par défaut',
-                    'salaire' => $_POST['salaire'] ?? 0,
-                    'teletravail' => $_POST['teletravail'] ?? 0,
-                    'duree' => $_POST['duree'] ?? 0,
-                    'id_etude' => $_POST['id_etude'] ?? 0,
-                    'id_contrat' => $_POST['id_contrat'] ?? 0,
-                    'id_secteur' => $_POST['id_secteur'] ?? 0,
-                    'id_entreprise' => $_POST['id_entreprise'] ?? 0
+                    'id_offres' => htmlspecialchars($_POST['offer_id-upd']),
+                    'titre' => htmlspecialchars($_POST['titre'] ?? 'Titre par défaut'),
+                    'description' => htmlspecialchars($_POST['description'] ?? 'Description par défaut'),
+                    'salaire' => htmlspecialchars($_POST['salaire'] ?? 0),
+                    'teletravail' => htmlspecialchars($_POST['teletravail'] ?? 0),
+                    'duree' => htmlspecialchars($_POST['duree'] ?? 0),
+                    'id_etude' => htmlspecialchars($_POST['id_etude'] ?? 0),
+                    'id_contrat' => htmlspecialchars($_POST['id_contrat'] ?? 0),
+                    'id_secteur' => htmlspecialchars($_POST['id_secteur'] ?? 0),
+                    'id_entreprise' => htmlspecialchars($_POST['id_entreprise'] ?? 0)
                 ];
                 $dbOffer->update($offre);
-
+            
             } elseif (isset($_POST['offer_id-suprWish'])) {
                 $dbwish = new Wishlist();
-                $dbwish->removeWish([$_SESSION["user_id"], $_POST['offer_id-suprWish']]);
+                $dbwish->removeWish([$_SESSION["user_id"], htmlspecialchars($_POST['offer_id-suprWish'])]);
+            
             } elseif (isset($_POST['offer_id-addWish'])) {
                 $dbwishs = new Wishlist();
-                $dbwishs->addWish([$_SESSION["user_id"], $_POST['offer_id-addWish']]);
+                $dbwishs->addWish([$_SESSION["user_id"], htmlspecialchars($_POST['offer_id-addWish'])]);
             }
-
+        
             $_POST = [];
             $_FILES = [];
         }
+        
 
 
 
